6960a75b74bae3c9a43c954a96541386152305ba
toasa <atsushi.tohyama.160.333@gmail.com>
2018-12-19 22:39:23 +0900

    今日の進捗

--- DPL/1_B.cpp
+++ DPL/1_B.cpp
@@ -3,6 +3,32 @@
 using namespace std;
 
 int main() {
+    int N, W;
+    int value[110];
+    int weight[110];
+
+    cin >> N >> W;
+    for (int i = 0; i < N; i++) {
+        cin >> value[i] >> weight[i];
+    }
+
+    int dp[110][10010];
+
+    for (int w = 0; w <= W; w++) {
+        dp[0][w] = 0;
+    }
+
+    for (int i = 0; i < N; i++) {
+        for (int w = 1; w <= W; w++) {
+            if (weight[i] < w) {
+                dp[i+1][w] = max(dp[i][w-weight[i]] + value[i], dp[i][w]);
+            } else {
+                dp[i+1][w] = dp[i][w];
+            }
+        }
+    }
+
+    cout << dp[N][W] << endl;
 
     return 0;
 }
