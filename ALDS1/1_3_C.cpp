a23e315d2879194e25c1ee5d0141c97c96cd8ea7
遠山篤史 <atsushi.tohyama.160.333@gmail.com>
2019-06-12 23:19:46 +0900

    Update ALDS1_3_C.cpp

--- ALDS1/1_3_C.cpp
+++ ALDS1/1_3_C.cpp
@@ -15,46 +15,118 @@
 
 using namespace std;
 
+typedef struct Node {
+    struct Node *prev;
+    struct Node *next;
+    int val;
+} node;
+
+// insert x
+// delete x
+// deleteFirst
+// deleteLast
+
+
+node* newNode(int val) {
+    node *n = new(node);
+    n->val = val;
+    n->next = NULL;
+    n->prev = NULL;
+    return n;
+}
+
+// 7
+// insert 1000000000
+// insert 999999999
+// deleteLast
+// insert 1234566890
+// insert 5
+// deleteFirst
+// insert 7
+// delete 5
+
 int main(){
-    int n;
-    cin >> n;
+    int num;
+    cin >> num;
 
-    deque<int> deq;
-    string str;
-    int m;
+    // Doubly Linked List
+    //  [] <-> [] <-> [] ... [] <-> []
+    // head                        tail
+    node *head = NULL;
+    node *tail = NULL;
+    string inst;
+    int val;
 
-    for(int i=0; i<n; i++){
-        cin >> str;
-        if(str=="insert"){
-            cin >> m;
-            deq.push_front(m);
-        }else if(str=="deleteFirst"){
-            deq.pop_front();
-        }else if(str=="deleteLast"){
-            deq.pop_back();
-        }else if(str=="delete"){
-            // 一番めんどくさい処理なので、最後に書いた
-            cin >> m;
-            deque<int>::iterator itr = deq.begin();
-            while(itr != deq.end()){
-                if(*itr==m){
-                    deq.erase(itr);
+    for (int i = 0; i < num; i++) {
+        cin >> inst;
+        if (inst == "insert") {
+            cin >> val;
+            node *n = newNode(val);
+
+            if (head != NULL) {
+                head->prev = n;
+                n->next = head;
+                head = n;
+            } else {
+                head = n;
+                tail = n;
+            }
+        } else if (inst == "delete") {
+            cin >> val;
+
+            node *n = head;
+            bool not_found = false;
+            while (n->val != val) {                
+                if (n == tail) {
+                    not_found = true;
                     break;
                 }
-                itr++;
+                n = n->next;
+            }
+            // 見つからなかった
+            if (not_found) {
+                continue;
             }
 
+            if (n->prev != NULL && n->next != NULL) {
+                n->prev->next = n->next;
+                n->next->prev = n->prev;
+            } else if (n->prev == NULL && n->next != NULL) {
+                head = n->next;
+                head->prev = NULL;
+            } else if (n->prev != NULL && n->next == NULL) {
+                tail = n->prev;
+                tail->next = NULL;
+            } else {
+                head = NULL;
+                tail = NULL;
+            }
+        } else if (inst == "deleteFirst") {
+            if (head->next != NULL) {
+                head = head->next;
+                head->prev = NULL;
+            } else {
+                head = NULL;
+                tail = NULL;
+            }
+        } else if (inst == "deleteLast") {
+            if (tail->prev != NULL) {
+                tail = tail->prev;
+                tail->next = NULL;
+            } else {
+                head = NULL;
+                tail = NULL;
+            }      
         }
     }
 
-    int d_s = deq.size();
-    for(int i=0; i<d_s; i++){
-        if(i==d_s-1){
-            cout << *deq.begin() << endl;
+    for (node *n = head; ; n = n->next) {
+        if (n == tail) {
+            cout << n->val << endl;
             break;
+        } else {
+            cout << n->val << " ";
         }
-        cout << *deq.begin() << " ";
-        deq.pop_front();
     }
 
     return 0;
